93e762a9624c21fa590263b48b2fcd560492337b
JeyyJeyy <coryocraft@gmail.com>
2022-07-01 16:45:07 +0200

    handling post request

---
+++ index.js
@@ -0,0 +1,19 @@
+var bodyParser = require('body-parser');
+
+const express = require('express');
+
+const app = express();
+
+// Parses the body for POST, PUT, DELETE, etc.
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/posts', function(req, res, next){
+
+    console.log(req.body); // req.body contains the parsed body of the request.
+
+});
+
+app.listen(8080, '192.168.1.249');
+console.log('LISTENNING PORT 8080')
