44bdbf5dfe339004acbd3967f5757e00e17312fd
JeyyJeyy <coryocraft@gmail.com>
2022-08-11 14:52:39 +0200

    updated web page

--- index.js
+++ index.js
@@ -137,6 +137,22 @@ function buildHtml(id,dat) {
             index = das.indexOf(value);
             if (x == 0 && index != null) {
                 x++
+                let date = product.date.replace(/\//gi,'_');
+         let time = product.date.split('/');
+         const date1 = Date.parse(time[1]+' '+time[0]+' '+time[2])
+         const date2 = Date.now();
+         const diffTime = Math.abs(date2 - date1);
+         let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
+         if(diffDays == 0 || diffDays == 1){
+            diffDays = 'aujourd\'hui';
+            url = 'equal.png';
+         }else if(date1 <= date2){
+            diffDays = 'depuis '+diffDays+' jours';
+            url = 'no.png';
+         }else{
+            diffDays = 'dans '+diffDays+' jours';
+            url = 'ok.png';
+         }
                 file = '<html lang="fr">'+
                 '<head>'+
                    '<meta charset="UTF-8">'+
@@ -158,10 +174,9 @@ function buildHtml(id,dat) {
                          '</tr>'+
                          '<tr>'+
                             '<td style="color:#555555; font-size: 12px;"> <img style="border-radius: 15px; height: 150px; width: 150px; object-fit: contain;" src='+das[index].lien+'> <br> '+das[index].barcode+' </td>'+
-                            '<td style="color:#555555; font-size: 16px;">${product.nom}</td>'+
-                            '<td style="color:#555555; font-size: 16px;">${product.date}<br>${diffDays}</td>'+
-                            '<td style="color:#555555; font-size: 16px;">${product.quantity}</td>'+
-                            '<td><button style="width: 90px; height: 30px; color:#555555; font-size: 16px; border-radius: 5px; border: 1px solid" onclick="added(${product.barcode},${date})"><b>Ajouter</b></button><br><br><button style="width: 90px; height: 30px; color:#555555; font-size: 16px; border-radius: 5px; border: 1px solid" onclick="delet(${product.barcode},${date})"><b>Supprimer</b></button><br><br><button style="width: 90px; height: 30px; color:#555555; font-size: 16px; border-radius: 5px; border: 1px solid" onclick="getinfos(${product.barcode},${date})"><b>Infos</b></button></td>'+
+                            '<td style="color:#555555; font-size: 16px;">'+das[index].nom+'</td>'+
+                            '<td style="color:#555555; font-size: 16px;">'+das[index].date+'<br>'+diffDays+'</td>'+
+                            '<td style="color:#555555; font-size: 16px;">'+das[index].quantity+'</td>'+
                          '</tr>'+
                       '</thead>'+
                       '<tbody id="data-output">'+
