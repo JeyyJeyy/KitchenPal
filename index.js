45d99386489f3fc61ad0c9ed3a2cd87eba17f93d
JeyyJeyy <coryocraft@gmail.com>
2022-08-11 14:58:40 +0200

    better product page

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ app.use(express.static('webpage'));
 app.use(express.static('webpage/icons/'));
 app.use(express.static('.'));
 
-app.get('/element.html', function(req, res) {
-    var html = buildHtml(req.query.id,req.query.date);
+app.get('/element.html', function (req, res) {
+    var html = buildHtml(req.query.id, req.query.date);
     res.end(html);
 })
-app.get('/element.css', function(req, res) {
+app.get('/element.css', function (req, res) {
     res.sendFile('element.css', { root: './webpage/element/' });
 })
 
@@ -38,34 +38,34 @@ app.post('/posts', function (req, res, next) {
         let index;
         if (req.body.command == 'add') {
             (async () => {
-            delete req.body["command"];
-            for (let dat of obj) {
-                if (dat.barcode == req.body.barcode && dat.date == req.body.date) {
-                    index = obj.indexOf(dat);
+                delete req.body["command"];
+                for (let dat of obj) {
+                    if (dat.barcode == req.body.barcode && dat.date == req.body.date) {
+                        index = obj.indexOf(dat);
+                    }
                 }
-            }
-            if (index != null) {
-                obj[index].quantity = obj[index].quantity + num;
-            } else {
-                await axios.get(`https://fr.openfoodfacts.org/api/v0/product/${req.body.barcode}.json`)
-                    .then(res => {
-                        let name = res.data.product.product_name_fr;
-                        let url = res.data.product.selected_images.front.display.fr;
-                        req.body["nom"] = name;
-                        req.body["lien"] = url;
-                        console.log(req.body);
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-                console.log(req.body)
-                obj.push(req.body);
-            }
-            json = JSON.stringify(obj);
-            fs.writeFile("data.json", json, (err) => {
-                if (err) console.log(err);
-            });
-            console.log("\x1b[36m", "[" + process.uptime().toFixed(2) + ' SAVE] Saved ' + num + ' elements to data.json');
+                if (index != null) {
+                    obj[index].quantity = obj[index].quantity + num;
+                } else {
+                    await axios.get(`https://fr.openfoodfacts.org/api/v0/product/${req.body.barcode}.json`)
+                        .then(res => {
+                            let name = res.data.product.product_name_fr;
+                            let url = res.data.product.selected_images.front.display.fr;
+                            req.body["nom"] = name;
+                            req.body["lien"] = url;
+                            console.log(req.body);
+                        })
+                        .catch(function (error) {
+                            console.log(error);
+                        });
+                    console.log(req.body)
+                    obj.push(req.body);
+                }
+                json = JSON.stringify(obj);
+                fs.writeFile("data.json", json, (err) => {
+                    if (err) console.log(err);
+                });
+                console.log("\x1b[36m", "[" + process.uptime().toFixed(2) + ' SAVE] Saved ' + num + ' elements to data.json');
             })();
         } else if (req.body.command == 'del') {
             delete req.body["command"];
@@ -125,66 +125,68 @@ function date(date) {
     return date[0] + '/' + date[1] + '/' + date[2];
 }
 
-function buildHtml(id,dat) {
+function buildHtml(id, dat) {
     let data = fs.readFileSync('data.json', 'utf8');
     let das = JSON.parse(data);
-    let date = dat.slice(0, 2)+'/'+dat.slice(2, 4)+'/'+dat.slice(4, 8);
+    let date2 = dat.slice(0, 2) + '/' + dat.slice(2, 4) + '/' + dat.slice(4, 8);
     let index;
     let file;
     let x = 0;
     das.forEach(function (value) {
-        if (value.barcode == id && value.date == date) {
+        if (value.barcode == id && value.date == date2) {
             index = das.indexOf(value);
             if (x == 0 && index != null) {
                 x++
-                let date = product.date.replace(/\//gi,'_');
-         let time = product.date.split('/');
-         const date1 = Date.parse(time[1]+' '+time[0]+' '+time[2])
-         const date2 = Date.now();
-         const diffTime = Math.abs(date2 - date1);
-         let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)); 
-         if(diffDays == 0 || diffDays == 1){
-            diffDays = 'aujourd\'hui';
-            url = 'equal.png';
-         }else if(date1 <= date2){
-            diffDays = 'depuis '+diffDays+' jours';
-            url = 'no.png';
-         }else{
-            diffDays = 'dans '+diffDays+' jours';
-            url = 'ok.png';
-         }
-                file = '<html lang="fr">'+
-                '<head>'+
-                   '<meta charset="UTF-8">'+
-                   '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-                   '<link rel="stylesheet" href="styles.css">'+
-                   '<link rel="icon" href="icon.ico" />'+
-                   '<title>Stock-Manager</title>'+
-                '</head>'+
-                '<body>'+
-                   '<br>'+
-                   '<center><img width="100" height="100" src="icon.ico"><h1>Stock Manager v1.2.0</h1></center>'+
-                   '<table>'+
-                      '<thead>'+
-                         '<tr>'+
-                            '<th>Image</th>'+
-                            '<th>Nom du produit</th>'+
-                            '<th>Date limite</th>'+
-                            '<th>Quantité</th>'+
-                         '</tr>'+
-                         '<tr>'+
-                            '<td style="color:#555555; font-size: 12px;"> <img style="border-radius: 15px; height: 150px; width: 150px; object-fit: contain;" src='+das[index].lien+'> <br> '+das[index].barcode+' </td>'+
-                            '<td style="color:#555555; font-size: 16px;">'+das[index].nom+'</td>'+
-                            '<td style="color:#555555; font-size: 16px;">'+das[index].date+'<br>'+diffDays+'</td>'+
-                            '<td style="color:#555555; font-size: 16px;">'+das[index].quantity+'</td>'+
-                         '</tr>'+
-                      '</thead>'+
-                      '<tbody id="data-output">'+
-                      '</tbody>'+
-                   '</table>'+
-                '</body>'+
-                '</html>';
-            }else{
+                let date = das[index].date.replace(/\//gi, '_');
+                let time = das[index].date.split('/');
+                const date1 = Date.parse(time[1] + ' ' + time[0] + ' ' + time[2])
+                const date2 = Date.now();
+                const diffTime = Math.abs(date2 - date1);
+                let diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+                if (diffDays == 0 || diffDays == 1) {
+                    diffDays = 'aujourd\'hui';
+                    url = 'equal.png';
+                } else if (date1 <= date2) {
+                    diffDays = 'depuis ' + diffDays + ' jours';
+                    url = 'no.png';
+                } else {
+                    diffDays = 'dans ' + diffDays + ' jours';
+                    url = 'ok.png';
+                }
+                file = '<html lang="fr">' +
+                    '<head>' +
+                    '<meta charset="UTF-8">' +
+                    '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+                    '<link rel="stylesheet" href="styles.css">' +
+                    '<link rel="icon" href="icon.ico" />' +
+                    '<title>Stock-Manager</title>' +
+                    '</head>' +
+                    '<body>' +
+                    '<br>' +
+                    '<center><img width="100" height="100" src="icon.ico"><h1>Stock Manager v1.2.0</h1></center>' +
+                    '<table>' +
+                    '<thead>' +
+                    '<tr>' +
+                    '<th>Consommable</th>' +
+                    '<th>Image</th>' +
+                    '<th>Nom du produit</th>' +
+                    '<th>Date limite</th>' +
+                    '<th>Quantité</th>' +
+                    '</tr>' +
+                    '<tr>' +
+                    '<td><img src=' + url + '></td>' +
+                    '<td style="color:#555555; font-size: 12px;"> <img style="border-radius: 15px; height: 150px; width: 150px; object-fit: contain;" src=' + das[index].lien + '> <br> ' + das[index].barcode + ' </td>' +
+                    '<td style="color:#555555; font-size: 16px;">' + das[index].nom + '</td>' +
+                    '<td style="color:#555555; font-size: 16px;">' + das[index].date + '<br>' + diffDays + '</td>' +
+                    '<td style="color:#555555; font-size: 16px;">' + das[index].quantity + '</td>' +
+                    '</tr>' +
+                    '</thead>' +
+                    '<tbody id="data-output">' +
+                    '</tbody>' +
+                    '</table>' +
+                    '</body>' +
+                    '</html>';
+            } else {
                 file = '<!DOCTYPE html><html><body><h1>TEST FAILED</h1></body></html>';
             }
         }
