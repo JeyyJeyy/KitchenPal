8a7b72dce494e6d9a5d6b12efe0d23b970ba5b50
JeyyJeyy <coryocraft@gmail.com>
2022-08-14 16:10:33 +0200

    darkmode now sync through pages

--- webpage/app.js
+++ webpage/app.js
@@ -1,3 +1,4 @@
+var dm = false;
 function delet(bar, date) {
    const response = fetch('/posts', {
       method: 'POST',
@@ -37,8 +38,34 @@ function getinfos(bar, date) {
 function darkmode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
+   if (dm == true) {
+      dm = false;
+   } else {
+      dm = true;
+   }
+   toggleModes(dm)
 }
 
 function gohome() {
    location.href = 'home.html';
+}
+
+function toggleModes(bool) {
+   if (new Number(bool) == 0) {
+      localStorage.setItem("isDarkMode", "0")
+   } else if (new Number(bool) == 1) {
+      localStorage.setItem("isDarkMode", "1")
+   } else {
+      localStorage.setItem("isDarkMode", "1")
+   }
+}
+
+if (localStorage.getItem("isDarkMode") == null) {
+   toggleModes(dm)
+}
+
+if (localStorage.getItem("isDarkMode") == "1" && dm != true) {
+   darkmode();
+} else if (localStorage.getItem("isDarkMode") == "0" && dm != false) {
+   darkmode();
 }
