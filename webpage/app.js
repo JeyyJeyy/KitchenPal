61dfcf42aff088de4748252aac67d559986a01d8
JeyyJeyy <coryocraft@gmail.com>
2022-08-14 22:47:07 +0200

    added search bar

--- webpage/app.js
+++ webpage/app.js
@@ -73,3 +73,22 @@ if (localStorage.getItem("isDarkMode") == "1" && dm != true) {
    darkmode();
    checkbox.checked = false;
 }
+
+function search() {
+   var input, filter, table, tr, td, i, txtValue;
+   input = document.getElementById("input");
+   filter = input.value.toUpperCase();
+   table = document.getElementById("data-output");
+   tr = table.getElementsByTagName("tr");
+   for (i = 0; i < tr.length; i++) {
+     td = tr[i].getElementsByTagName("td")[2];
+     if (td) {
+       txtValue = td.textContent || td.innerText;
+       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+         tr[i].style.display = "";
+       } else {
+         tr[i].style.display = "none";
+       }
+     }
+   }
+ }
