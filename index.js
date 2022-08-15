808e69ccbad47cee8c570b3e23f05865644624bf
JeyyJeyy <coryocraft@gmail.com>
2022-08-15 12:32:47 +0200

    updated to 1.7.10

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ app.post('/posts', function (req, res, next) {
 });
 
 app.listen(8080, 'localhost', () => {
-    console.log("\x1b[1m", 'Stock-Manager v1.6.0: [Serveur allumé sur le port 8080]')
+    console.log("\x1b[1m", 'Stock-Manager v1.7.10: [Serveur allumé sur le port 8080]')
 })
 
 
@@ -167,7 +167,7 @@ function buildHtml(id, dat) {
     let date2 = dat.slice(0, 2) + '/' + dat.slice(2, 4) + '/' + dat.slice(4, 8);
     let index;
     let file = '<html lang="fr"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="styles.css"><link rel="icon" href="icon.ico" /><title>Stock-Manager</title></head>' +
-        '<body><br><label><input name="mode" type="checkbox" style="width: 20px; height: 20px;" onclick="darkmode()">  Dark mode</label><br><center><a href="/home.html"><img width="100" height="100" src="icon.ico"></a><h1>Stock Manager v1.6.0</h1></center><br>';
+        '<body><br><label><input name="mode" type="checkbox" style="width: 20px; height: 20px;" onclick="darkmode()">  Dark mode</label><br><center><a href="/home.html"><img width="100" height="100" src="icon.ico"></a><h1>Stock Manager v1.7.10</h1></center><br>';
     let x = 0;
     das.forEach(function (value) {
         if (value.barcode == id && value.date == date2) {
