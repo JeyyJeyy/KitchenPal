d296e88330c7f631c90535939552584f49e66770
JeyyJeyy <coryocraft@gmail.com>
2022-12-27 19:04:59 +0100

    Updated port

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ app.post('/posts', function (req, res, next) {
     });
 });
 
-app.listen(8080, '192.168.0.6', () => {
+app.listen(8080, () => {
     console.log("\x1b[1m", 'Stock-Manager v2.6.3: [Server enabled on port 8080]')
 })
 
